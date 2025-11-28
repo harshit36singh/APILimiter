4dddd525a36c007470337e561750e4fc3d20a106
harshit36singh <singh36harshit@gmail.com>
2025-11-28 16:15:12 +0530

    made the springboot and react files

---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/ApilimiterApplication.java
@@ -0,0 +1,13 @@
+package com.example.apilimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilimiterApplication.class, args);
+	}
+
+}
---
+++ Backend/apilimiter/src/test/java/com/example/apilimiter/ApilimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apilimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
