94c857ec94c6d592d7b3baf446b213a19c6ac9b1
harshit36singh <singh36harshit@gmail.com>
2025-12-05 15:54:42 +0530

    added a projectresponse and request dto

---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/dto/ProjectRequest.java
@@ -0,0 +1,11 @@
+package com.example.apilimiter.dto;
+
+import lombok.Data;
+
+@Data
+public class ProjectRequest {
+    private String name;
+    private String desc;
+    
+
+}
---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/dto/ProjectResponse.java
@@ -0,0 +1,14 @@
+package com.example.apilimiter.dto;
+
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class ProjectResponse {
+    private Long id;
+    private String name;
+    private String desc;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
