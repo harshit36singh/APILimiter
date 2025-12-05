72ebf1aa36a0d8d1de5129e4edaacc8689c84b1b
harshit36singh <singh36harshit@gmail.com>
2025-12-05 15:36:02 +0530

    added the project entity

---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/entities/Project.java
@@ -0,0 +1,54 @@
+package com.example.apilimiter.entities;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition="TEXT")
+    private String desc;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="owner_id")
+    @JsonIgnore
+    private User owner;
+
+
+    @CreatedDate
+    private Instant createdAt=Instant.now();
+
+
+
+    @LastModifiedDate
+    private Instant updatedAt=Instant.now();
+
+
+
+
+}
