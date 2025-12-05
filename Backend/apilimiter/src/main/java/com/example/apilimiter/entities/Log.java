23f7773c4de5952862c3115928d95be947861d1b
harshit36singh <singh36harshit@gmail.com>
2025-12-05 20:07:31 +0530

    made the log entity with its repository and a method to put the log in time order

---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/entities/Log.java
@@ -0,0 +1,34 @@
+package com.example.apilimiter.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="visit_logs")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long projectId;
+
+    private String ipAddress;
+
+    private Instant timestamp;
+    
+
+
+
+}
