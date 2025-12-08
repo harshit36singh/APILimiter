d7a789a9b1e1030958b241bf666b4050cdb87f7a
harshit36singh <singh36harshit@gmail.com>
2025-12-08 16:08:26 +0530

    added the clientuseapicontroller

--- Backend/apilimiter/src/main/java/com/example/apilimiter/controller/ClientUseAPIController.java
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/controller/ClientUseAPIController.java
@@ -1,7 +1,12 @@
 package com.example.apilimiter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.apilimiter.service.ClientUseAPIService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +14,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/apilimiter")
 @RequiredArgsConstructor
 public class ClientUseAPIController {
-    
+    private final ClientUseAPIService clientUseAPIService;
+
+
+    @GetMapping("/{name}")
+    public ResponseEntity<?> publicApiResult(@PathVariable("name") String shortname){
+        Object object=clientUseAPIService.fetchByShortname(shortname);
+        return ResponseEntity.ok(object);
+
+    }
     
 }
