2cf59826bd08d4269c325edf18e658b4dc51b657
harshit36singh <singh36harshit@gmail.com>
2025-12-08 15:27:42 +0530

    added the clientuseapi for the people using it to view

---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/controller/ClientUseAPIController.java
@@ -0,0 +1,13 @@
+package com.example.apilimiter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/apilimiter")
+@RequiredArgsConstructor
+public class ClientUseAPIController {
+    
+}
---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/service/ClientUseAPIService.java
@@ -0,0 +1,11 @@
+package com.example.apilimiter.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ClientUseAPIService {
+    
+}
