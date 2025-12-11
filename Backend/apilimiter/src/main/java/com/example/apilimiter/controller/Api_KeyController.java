05c79eaf0ad8cb4a64c56c456a6ca2421c3232ab
harshit36singh <singh36harshit@gmail.com>
2025-12-12 01:38:22 +0530

    completed the apikeycontroller

--- Backend/apilimiter/src/main/java/com/example/apilimiter/controller/Api_KeyController.java
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/controller/Api_KeyController.java
@@ -1,7 +1,15 @@
 package com.example.apilimiter.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.apilimiter.entities.User;
 import com.example.apilimiter.service.Api_KeyService;
 import com.example.apilimiter.util.UserAuthHelper;
 
@@ -9,9 +17,32 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/apikeys")
 public class Api_KeyController extends UserAuthHelper{
     
     private final Api_KeyService api_keyservice;
 
+    @PostMapping("/{id}")
+    public ResponseEntity<?> generatekey(@PathVariable("id") Long projectid,Authentication auth){
+        User owner=(User)getUser(auth);
+        return ResponseEntity.ok(api_keyservice.createkey(owner, projectid));
     
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listofkeysforaparticularproject(@PathVariable("id") Long projectId,Authentication auth){
+        User owner=(User)getUser(auth);
+        return ResponseEntity.ok(api_keyservice.getallapikeyforaparticularproject(owner, projectId));
+    
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> disablekey(@PathVariable("id") Long keyid,Authentication auth){
+        User owner=(User)getUser(auth);
+        api_keyservice.disablekey(owner, keyid);
+        return ResponseEntity.noContent().build();
+        
+    }
+
+
 }
