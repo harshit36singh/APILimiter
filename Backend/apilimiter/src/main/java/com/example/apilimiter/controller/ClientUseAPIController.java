f5a5faeecf5af785a1e7a6f59231a2f0edea1e90
harshit36singh <singh36harshit@gmail.com>
2025-12-12 01:51:32 +0530

    added the verification for the api_key in the clientusecallapi so that a person with a different project can't call a different api .

--- Backend/apilimiter/src/main/java/com/example/apilimiter/controller/ClientUseAPIController.java
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/controller/ClientUseAPIController.java
@@ -3,11 +3,14 @@ package com.example.apilimiter.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.apilimiter.entities.Api_Key;
 import com.example.apilimiter.entities.Project;
 import com.example.apilimiter.repositories.ProjectRepo;
+import com.example.apilimiter.service.Api_KeyService;
 import com.example.apilimiter.service.ClientUseAPIService;
 import com.example.apilimiter.service.LogService;
 
@@ -21,20 +24,29 @@ public class ClientUseAPIController {
    private final ClientUseAPIService clientUseAPIService;
     private final LogService logService;
     private final ProjectRepo projectRepo;
+    private final Api_KeyService api_KeyService;
 
 
      @GetMapping("/{shortname}")
     public ResponseEntity<?> publicApiResult(
             @PathVariable String shortname,
+            @RequestHeader("Authorization") String apiheader,
             HttpServletRequest request
     ) {
 
-       
+       String apikey=apiheader.replace("apik_", "");
+
+       Api_Key key=api_KeyService.validatkey(apikey);
         Project project = projectRepo.findByShortname(shortname)
                 .orElseThrow(() -> new RuntimeException("Project not found"));
 
+        if(!key.getProject().getId().equals(project.getId())){
+                throw new IllegalArgumentException("This api_key doesn't belong to this project .");
+        }        
+
         String ip = request.getRemoteAddr();
         logService.logvisit(project.getId(),ip);
+
 
        
         Object result = clientUseAPIService.fetch(project.getApi_url());
