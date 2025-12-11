b74752179d6acb7760352e836f02372b87b20ef9
harshit36singh <singh36harshit@gmail.com>
2025-12-12 00:19:01 +0530

    completed the apikey generator

--- Backend/apilimiter/src/main/java/com/example/apilimiter/util/Api_Key_Generator.java
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/util/Api_Key_Generator.java
@@ -1,8 +1,12 @@
 package com.example.apilimiter.util;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class Api_Key_Generator {
-    
+    public String generateapikey(){
+        return "apik_"+UUID.randomUUID().toString().replace("-", "");
+    }
 }
