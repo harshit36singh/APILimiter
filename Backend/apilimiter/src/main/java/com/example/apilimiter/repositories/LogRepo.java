674684d88b1a876e368816c510c7015d289c3cd3
harshit36singh <singh36harshit@gmail.com>
2025-12-18 14:03:48 +0530

    added usageservice

--- Backend/apilimiter/src/main/java/com/example/apilimiter/repositories/LogRepo.java
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/repositories/LogRepo.java
@@ -3,8 +3,11 @@ package com.example.apilimiter.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.apilimiter.dto.Api_KeyResponseDto;
 import com.example.apilimiter.entities.Log;
 
 @Repository
@@ -12,4 +15,18 @@ public interface LogRepo extends JpaRepository<Log,Long>{
     
 List<Log> findByProjectIdOrderByTimestampDesc(Long projectId);
 
+
+@Query("""
+    Select new com.example.apilimiter.dto.Api_keyResponseDto(
+    k.id,
+    k.apikey,
+    COUNT(k.totalreq),
+    MAX(k.lastusedtime)
+    )
+    from Log l
+    Join l.api_key k
+    where l.projectid=:projectid
+    Group By k.id,k.keyValue  
+        """)
+List<Api_KeyResponseDto> getusagebyProject(@PathVariable("projectid") Long projectid);
 }
---
+++ Backend/apilimiter/src/main/java/com/example/apilimiter/service/UsageService.java
@@ -0,0 +1,21 @@
+package com.example.apilimiter.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.apilimiter.dto.Api_KeyResponseDto;
+import com.example.apilimiter.repositories.LogRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsageService {
+    
+    private final LogRepo logRepo;
+
+    public List<Api_KeyResponseDto> getallprojectsusage(Long projectid){
+        return logRepo.getusagebyProject(projectid);
+    }
+}
